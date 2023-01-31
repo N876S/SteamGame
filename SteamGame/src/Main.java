e7ff0a03097563f55f4fdb5fac5291d5e6c28c09
nsmall <nsmall@Nathans_PC>
2023-01-30 20:41:32 -0500

    Window uwu

--- SteamGame/src/Main.java
+++ SteamGame/src/Main.java
@@ -1,6 +1,18 @@
+import javax.swing.JPanel;
 
-public class Main {
+public class Main extends JPanel implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
+	Game game = new Game();
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Main main = new Main();
+		new Window(main);
 	}
+
+	public void run() {
+		
+	}
+	
+	
 }
